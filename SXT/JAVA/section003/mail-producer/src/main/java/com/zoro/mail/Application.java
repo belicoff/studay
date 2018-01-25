67883a2a167ca7d7eaf381ba719f116a76959657
zhaoguangfu <zhaoguangfu@jd.com>
2018-01-25 14:20:23 +0800

    过滤数据源自动配置

--- SXT/JAVA/section003/mail-producer/src/main/java/com/zoro/mail/Application.java
+++ SXT/JAVA/section003/mail-producer/src/main/java/com/zoro/mail/Application.java
@@ -2,11 +2,12 @@ package com.zoro.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * spring boot  主入口
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
